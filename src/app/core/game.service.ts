18a789ef45c0e5657af21568b3fe3830da9eea71
Ilia Idakiev <ilia.idakiev@gmail.com>
2017-03-25 10:44:08 +0200

    hardcoded columns and cells

--- src/app/core/game.service.ts
+++ src/app/core/game.service.ts
@@ -67,14 +67,28 @@ export class GameService {
   }
 
   initializeGame() {
-     Observable.from(this.cells)
-      .bufferCount(4)
-      .bufferCount(4)
-      .do(rows => this.rows = rows)
-      .map(rows => rows.map(row => Observable.from(row)))
-      .switchMap(obsArray => Observable.zip(...obsArray))
-      .bufferCount(4)
-      .subscribe(columns => this.columns = columns);
+    //  Observable.from(this.cells)
+    //   .bufferCount(4)
+    //   .bufferCount(4)
+    //   .do(rows => this.rows = rows)
+    //   .map(rows => rows.map(row => Observable.from(row)))
+    //   .switchMap(obsArray => Observable.zip(...obsArray))
+    //   .bufferCount(4)
+    //   .subscribe(columns => this.columns = columns);
+
+    this.columns = [
+      [this.cells[0], this.cells[4], this.cells[8], this.cells[12]],
+      [this.cells[1], this.cells[5], this.cells[9], this.cells[13]],
+      [this.cells[2], this.cells[6], this.cells[10], this.cells[14]],
+      [this.cells[3], this.cells[7], this.cells[11], this.cells[15]],
+    ];
+
+    this.rows = [
+      [this.cells[0], this.cells[1], this.cells[2], this.cells[3]],
+      [this.cells[4], this.cells[5], this.cells[6], this.cells[7]],
+      [this.cells[8], this.cells[9], this.cells[10], this.cells[11]],
+      [this.cells[12], this.cells[13], this.cells[14], this.cells[15]],
+    ];
   }
 
   constructor() {
